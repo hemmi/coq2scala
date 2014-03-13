605162be2d3f0c1145981aeed1b2ecb22c45fa57
Ko Hemmi <churulinko@gmail.com>
2014-03-14 03:52:58 +0900

    undo

--- coq-8.4pl2/plugins/extraction/table.mli
+++ coq-8.4pl2/plugins/extraction/table.mli
@@ -4,7 +4,8 @@
 (*   \VV/  **************************************************************)
 (*    //   *      This file is distributed under the terms of the       *)
 (*         *       GNU Lesser General Public License Version 2.1        *)
-(************************************************************************) 
+(************************************************************************)
+
 open Names
 open Libnames
 open Miniml
